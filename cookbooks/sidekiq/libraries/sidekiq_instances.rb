754dfa440a312000f4b2dc8265784ec8fa38be7b
@kylekeesling <kyle.keesling@gmail.com>
2013-04-02 22:22:30 -0400

    updated sidekiq to run on app servers

--- cookbooks/sidekiq/libraries/sidekiq_instances.rb
+++ cookbooks/sidekiq/libraries/sidekiq_instances.rb
@@ -3,7 +3,7 @@ class Chef
     # Does this instance run sidekiq?
     def sidekiq_instance?
       role = node[:instance_role]
-      role == 'solo' || role == 'eylocal' || (role == 'util' && node[:name] =~ /^sidekiq/)
+      role == 'app_master' || role == 'app' || role == 'solo' || role == 'eylocal' || (role == 'util' && node[:name] =~ /^sidekiq/)
     end
   end
 end
